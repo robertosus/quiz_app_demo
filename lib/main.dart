5ad1a6ed894e9a83657dd4d0b0db9d0033fc41f9
dowerkali <susiloroberto@yahoo.com>
2023-03-15 20:01:47 +0700

    Membuat function pada class menuProviders

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:quiz_app/providers/menu_provider.dart';
 import 'package:quiz_app/screens/home_screen.dart';
 
 import 'models/menuQuiz_model.dart';
@@ -19,11 +21,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
+    return ChangeNotifierProvider(
+        create: (context) => MenuProvider(),
+        child: MaterialApp(
+          theme: ThemeData(
+              textTheme:
+                  GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)),
+          debugShowCheckedModeBanner: false,
+          home: HomeScreen(),
+        ));
   }
 }
